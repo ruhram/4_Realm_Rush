a8791c97bcbf7ac1eaee856ab68904e9986d1fe9
LAPTOP-EAT55AGK\Oscarium <rullyrgandadiwirya@gmail.com>
2021-02-25 15:51:06 +0700

    Labelling the box

--- Assets/EditorSnap.cs
+++ Assets/EditorSnap.cs
@@ -6,6 +6,7 @@ using UnityEngine;
 public class EditorSnap : MonoBehaviour
 {
     [SerializeField] [Range(1f, 10f)] float gridSize = 10f;
+    TextMesh textMesh;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,8 +17,13 @@ public class EditorSnap : MonoBehaviour
     void Update()
     {
         Vector3 snapPos;
+
         snapPos.x = Mathf.RoundToInt(transform.position.x / gridSize) * gridSize;
         snapPos.z = Mathf.RoundToInt(transform.position.z / gridSize) * gridSize;
+
+        textMesh = GetComponentInChildren<TextMesh>();
+        textMesh.text = snapPos.x/gridSize + "," + snapPos.z/gridSize;
+
         transform.position = new Vector3(snapPos.x, 0f, snapPos.z);
 
     }

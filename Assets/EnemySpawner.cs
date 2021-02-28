5e558b83e1574439f00fe4b61a4b7323e6d8b179
LAPTOP-EAT55AGK\Oscarium <rullyrgandadiwirya@gmail.com>
2021-02-28 20:47:17 +0700

    Revise  Coroutine

---
+++ Assets/EnemySpawner.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    [SerializeField] float SecondBetweenSpawn = 2f;
+    [SerializeField] EnemyMovement enemyPrefab;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(RepeateadlySpawnEnemy());
+    }
+
+    IEnumerator RepeateadlySpawnEnemy()
+    {
+        while (true)
+        {
+            print("Spawned Enemy");
+            yield return new WaitForSeconds(SecondBetweenSpawn);//wait a bit
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

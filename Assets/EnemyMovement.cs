8065060d7cb901fdec4f383513a520426d0c2f2f
LAPTOP-EAT55AGK\Oscarium <rullyrgandadiwirya@gmail.com>
2021-02-28 13:01:14 +0700

    Collider of enemy

--- Assets/EnemyMovement.cs
+++ Assets/EnemyMovement.cs
@@ -19,7 +19,7 @@ public class EnemyMovement : MonoBehaviour
         foreach(Block waypoint in path)
         {
             transform.position = waypoint.transform.position;
-            yield return new WaitForSeconds(1f);
+            yield return new WaitForSeconds(2f);
         }
     }
 
